e6915213c9f372b771faed1e892fe1216254bffc
samuelhuang <pedoe1989@gmail.com>
2018-03-25 16:18:28 +0800

    Pedoe add 503 solution

---
+++ Solutions/503-NextGreaterElementII.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} findNums
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var nextGreaterElements = function(nums) {
+    let map = new Map()
+    let stack = []
+    let ans = []
+ 
+    for(let i = (nums.length*2)-1; i > 0; i--){
+        while(stack.length > 0 && (nums[i%nums.length] >= stack[stack.length-1])){
+            stack.pop()
+        }
+        ans[i%(nums.length)] = (stack.length === 0 ? -1 : stack[stack.length-1])
+        stack.push(nums[i % nums.length])
+    }
+    
+    return ans
+};
